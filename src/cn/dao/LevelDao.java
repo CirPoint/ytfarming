ed60fbe15a968898294533ed390345a325312b54
CirPoint <32691252+CirPoint@users.noreply.github.com>
2017-10-24 15:37:35 +0800

    Revert "Initial commit"
    This reverts commit d1eda83e4243fa9dc593bf38eafa33ea21180a6f.

--- src/cn/dao/LevelDao.java
+++
@@ -1,8 +0,0 @@
-package cn.dao;
-
-import cn.bean.Level;
-
-public interface LevelDao {
-	//根据级别id显示级别
-	public Level findLvById(int id);
-}
--- src/cn/dao/StateDao.java
+++
@@ -1,9 +0,0 @@
-package cn.dao;
-
-
-import cn.bean.State;
-
-public interface StateDao {
-	//根据状态id显示相应状态
-	public State findStByid(int id);
-}
